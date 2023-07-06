1b609b0e2b88d1dc13a889db3bd3297282247ad6
Roberto Valle <roberto.valle@upm.es>
2023-07-06 14:01:26 +0200

    Load categories from different files

--- categories/buildings.py
+++ categories/buildings.py
@@ -7,8 +7,15 @@ from images_framework.src.categories import Name, Category as Oi
 
 
 class Building(Oi):
+    # XView2
     Oi.BUILDING.UNCLASSIFIED = Name('Unclassified')
     Oi.BUILDING.NO_DAMAGE = Name('No damage')
     Oi.BUILDING.MINOR_DAMAGE = Name('Minor damage')
     Oi.BUILDING.MAJOR_DAMAGE = Name('Major damage')
     Oi.BUILDING.DESTROYED = Name('Destroyed')
+    # Xview
+    Oi.BUILDING.HUT_TENT = Name('Hut/tent')
+    Oi.BUILDING.SHED = Name('Shed')
+    Oi.BUILDING.AIRCRAFT_HANGAR = Name('Aircraft hangar')
+    Oi.BUILDING.DAMAGED_BUILDING = Name('Damaged building')
+    Oi.BUILDING.FACILITY = Name('Facility')
