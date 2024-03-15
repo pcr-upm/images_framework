945757f4dac7cd38e8a9be65061853e9c1e4ccaf
Roberto Valle <roberto.valle@upm.es>
2024-03-15 15:49:04 +0100

    Added FaceSynthetics database landmarks

--- src/datasets.py
+++ src/datasets.py
@@ -247,6 +247,41 @@ class WFLW(Database):
         return seq
 
 
+class FaceSynthetics(Database):
+    def __init__(self):
+        from images_framework.alignment.landmarks import FaceLandmarkPart as Pf
+        super().__init__()
+        self._names = ['face_synthetics']
+        self._landmarks = {Pf.LEYEBROW: (1, 119, 2, 121, 3), Pf.REYEBROW: (4, 124, 5, 126, 6), Pf.LEYE: (7, 138, 139, 8, 141, 142, 169), Pf.REYE: (11, 144, 145, 12, 147, 148, 170), Pf.NOSE: (128, 129, 130, 17, 16, 133, 134, 135, 18), Pf.TMOUTH: (20, 150, 151, 22, 153, 154, 21, 165, 164, 163, 162, 161), Pf.BMOUTH: (156, 157, 23, 159, 160, 168, 167, 166), Pf.LEAR: (101, 102, 103, 104, 105, 106), Pf.REAR: (112, 113, 114, 115, 116, 117), Pf.CHIN: (107, 108, 24, 110, 111)}
+        self._categories = {0: Oi.FACE}
+        self._colors = [(0, 255, 0)]
+
+    def load_filename(self, path, db, line):
+        import cv2
+        import itertools
+        from PIL import Image
+        from .annotations import PersonObject
+        from images_framework.alignment.landmarks import lps, PersonLandmarkPart as Pl
+        seq = GenericGroup()
+        parts = line.strip().split(';')
+        image = GenericImage(path + parts[0])
+        width, height = Image.open(image.filename).size
+        image.tile = np.array([0, 0, width, height])
+        obj = PersonObject()
+        obj.add_category(GenericCategory(Oi.FACE))
+        indices = [101, 102, 103, 104, 105, 106, 107, 108, 24, 110, 111, 112, 113, 114, 115, 116, 117, 1, 119, 2, 121, 3, 4, 124, 5, 126, 6, 128, 129, 130, 17, 16, 133, 134, 135, 18, 7, 138, 139, 8, 141, 142, 11, 144, 145, 12, 147, 148, 20, 150, 151, 22, 153, 154, 21, 156, 157, 23, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170]
+        for idx in range(0, len(indices)):
+            label = indices[idx]
+            lp = list(self._landmarks.keys())[next((ids for ids, xs in enumerate(self._landmarks.values()) for x in xs if x == label), None)]
+            pos = (float(parts[(2*idx)+3]), float(parts[(2*idx)+4]))
+            obj.add_landmark(GenericLandmark(label, lp, pos, True), lps[type(lp)])
+        obj.bb = cv2.boundingRect(np.array([[pt.pos for pt in list(itertools.chain.from_iterable(obj.landmarks[Pl.FACE.value].values()))]]).astype(int))
+        obj.bb = (obj.bb[0], obj.bb[1], obj.bb[0]+obj.bb[2], obj.bb[1]+obj.bb[3])
+        image.add_object(obj)
+        seq.add_image(image)
+        return seq
+
+
 class DAD(Database):
     def __init__(self):
         from images_framework.alignment.landmarks import FaceLandmarkPart as Pf
